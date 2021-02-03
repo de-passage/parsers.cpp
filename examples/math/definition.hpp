410c0a6e73dfcdad2ce134a5c89c42e74fea5370
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2021-02-03 10:03:20 +0800

    Moves the math parser to its own directory

---
+++ examples/math/definition.hpp
@@ -0,0 +1,53 @@
+#ifndef GUARD_EXAMPLE_MATH_DEFINITION_HPP
+#define GUARD_EXAMPLE_MATH_DEFINITION_HPP
+
+#include <parsers/parsers.hpp>
+
+namespace math {
+using namespace parsers::description;
+using namespace ascii;
+
+using opening_parenthese = character<'('>;
+using closing_parenthese = character<')'>;
+using plus = character<'+'>;
+using minus = character<'-'>;
+using whole_number = both<digit, many<digit>>;
+using number = either<both<minus, whole_number>, whole_number>;
+template <class... Ts>
+using parenthesised = sequence<opening_parenthese,
+                               many<whitespace>,
+                               Ts...,
+                               many<whitespace>,
+                               closing_parenthese>;
+
+struct rec_math_expression;
+
+using restricted_math_expression =
+    alternative<number, parenthesised<rec_math_expression>>;
+
+template <class Op>
+using binary_operation = sequence<restricted_math_expression,
+                                  many<whitespace>,
+                                  Op,
+                                  many<whitespace>,
+                                  rec_math_expression>;
+using plus_op = binary_operation<plus>;
+using minus_op = binary_operation<minus>;
+using operation = alternative<plus_op, minus_op>;
+
+struct rec_math_expression
+    : recursive<
+          alternative<operation, number, parenthesised<rec_math_expression>>> {
+};
+
+namespace detail {
+template <class... Ts>
+using math_seq =
+    sequence<many<whitespace>, rec_math_expression, many<whitespace>, Ts...>;
+}
+
+constexpr detail::math_seq<parsers::dsl::eos_t> math_expression;
+constexpr detail::math_seq<> open_ended_math_expression;
+}  // namespace math
+
+#endif  // GUARD_EXAMPLE_MATH_DEFINITION_HPP
---
+++ examples/math/main.cpp
@@ -0,0 +1,27 @@
+#include "./definition.hpp"
+
+#include <iostream>
+#include <string>
+
+using namespace std::literals::string_literals;
+using parsers::match;
+
+constexpr auto valid = [](const auto& str) {
+  return match(math::math_expression, str);
+};
+static_assert(!valid(""));
+static_assert(!valid(" "));
+static_assert(!valid("()"));
+static_assert(valid("  -42 "));
+static_assert(valid("18"));
+static_assert(valid("-18"));
+static_assert(valid("(1)"));
+static_assert(valid("(-42)"));
+static_assert(valid("1+1"));
+static_assert(valid("-32 + 14"));
+static_assert(valid("( 43 + -8) + 14"));
+static_assert(valid("1+1--2"));
+static_assert(!valid("1 + ( 1 + ( 2 - 1)"));
+static_assert(!valid("1 + 2)"));
+
+int main() {}
