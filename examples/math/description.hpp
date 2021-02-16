d19c2d45bc4aca82754bb08b55e929fed8d70a7b
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2021-02-16 12:55:53 +0800

    Restructure math_parser example & add parser modifiers

--- examples/math/description.hpp
+++ examples/math/description.hpp
@@ -1,7 +1,9 @@
 #ifndef GUARD_EXAMPLE_MATH_DEFINITION_HPP
 #define GUARD_EXAMPLE_MATH_DEFINITION_HPP
 
+#include <functional>
 #include <parsers/parsers.hpp>
+#include <type_traits>
 
 namespace math {
 using namespace parsers::description;
@@ -15,43 +17,53 @@ struct to_int {
     int acc = 0;
     while (beg != end) {
       acc = acc * 10 + (*beg - '0');
+      ++beg;
     }
     return acc;
   }
-  constexpr int operator()(std::variant<int, int>&& var) const noexcept {
+  template <
+      class V,
+      std::enable_if_t<std::is_same_v<std::decay_t<V>, std::variant<int, int>>,
+                       int> = 0>
+  constexpr int operator()(V&& var) const noexcept {
     if (var.index() == 0) {
       return std::get<0>(var) * -1;
     }
     return std::get<1>(var);
   }
 };
+template <class T>
+struct always {
+  template <class... Ts>
+  constexpr T operator()([[maybe_unused]] Ts&&...) const noexcept {
+    return T{};
+  }
+};
 
 using opening_parenthese = d<character<'('>>;
 using closing_parenthese = d<character<')'>>;
+using spaces = d<many<space_t>>;
 using plus = character<'+'>;
 using minus = character<'-'>;
 using whole_number = build<many1<digit_t>, to_int>;
-using number = either<both<d<minus>, whole_number>, whole_number>;
+using number = map<either<both<d<minus>, whole_number>, whole_number>, to_int>;
 template <class... Ts>
-using parenthesised = sequence<opening_parenthese,
-                               many<space_t>,
-                               Ts...,
-                               many<space_t>,
-                               closing_parenthese>;
+using parenthesised =
+    sequence<opening_parenthese, spaces, Ts..., spaces, closing_parenthese>;
 
 struct rec_math_expression;
 
 using restricted_math_expression =
     alternative<number, parenthesised<rec_math_expression>>;
 
-template <class Op>
+template <class Sym, class Op>
 using binary_operation = sequence<restricted_math_expression,
-                                  many<space_t>,
-                                  Op,
-                                  many<space_t>,
+                                  spaces,
+                                  map<Sym, always<Op>>,
+                                  spaces,
                                   rec_math_expression>;
-using plus_op = binary_operation<plus>;
-using minus_op = binary_operation<minus>;
+using plus_op = binary_operation<plus, std::plus<>>;
+using minus_op = binary_operation<minus, std::minus<>>;
 using operation = alternative<plus_op, minus_op>;
 
 struct rec_math_expression
@@ -61,8 +73,7 @@ struct rec_math_expression
 
 namespace detail {
 template <class... Ts>
-using math_seq =
-    sequence<many<space_t>, rec_math_expression, many<space_t>, Ts...>;
+using math_seq = sequence<spaces, rec_math_expression, spaces, Ts...>;
 }
 
 static_assert(is_sequence_v<detail::math_seq<>>);
---
+++ examples/math/static_tests.cpp
@@ -0,0 +1,38 @@
+#include "./description.hpp"
+
+using parsers::match;
+
+constexpr auto valid = [](const auto& str) {
+  return match(math::math_expression, str);
+};
+
+static_assert(!match(math::whole_number(), ""));
+static_assert(!valid(""));
+static_assert(!valid(" "));
+static_assert(!valid("()"));
+static_assert(valid("  -42 "));
+static_assert(valid("18"));
+static_assert(valid("-18"));
+static_assert(valid("(1)"));
+static_assert(valid("(-42)"));
+static_assert(valid("1+1"));
+static_assert(valid("-32 + 14"));
+static_assert(valid("( 43 + -8) + 14"));
+static_assert(valid("1+1--2"));
+static_assert(!valid("1 + ( 1 + ( 2 - 1)"));
+static_assert(!valid("1 + 2)"));
+
+constexpr auto check_number_parser = [](const auto& str, int v) -> bool {
+  auto r = parsers::parse(math::number{}, str);
+  if (!r.has_value()) {
+    return false;
+  }
+  return r.value() == v;
+};
+
+static_assert(check_number_parser("123", 123));
+static_assert(check_number_parser("-42", -42));
+static_assert(!check_number_parser("", 0));
+static_assert(!check_number_parser("  ", 0));
+static_assert(!check_number_parser("ab", 0));
+static_assert(!check_number_parser("-ab", 0));
